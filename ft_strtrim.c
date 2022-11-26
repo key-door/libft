e798c7e6a1c8d20d435bd685e2ddf175db1f6b56
key-door <101494707+key-door@users.noreply.github.com>
2022-11-27 07:44:59 +0900

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,38 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 22:23:54 by keys              #+#    #+#             */
-/*   Updated: 2022/10/06 16:10:37 by kyoda            ###   ########.fr       */
+/*   Updated: 2022/11/27 07:44:36 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*trim_back(char const *s1, char const *set);
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	len;
-	char	*ans;
-
-	if (!s1 || !set)
-		return (NULL);
-	len = ft_strlen(s1);
-	while (*s1 != '\0')
-	{
-		if (ft_strchr(set, (int)*s1) == NULL)
-			return (trim_back(s1, set));
-		s1++;
-		len--;
-	}
-	ans = malloc(1);
-	if (ans == NULL)
-		return (NULL);
-	*ans = '\0';
-	return (ans);
-}
 
 static char	*trim_back(char const *s1, char const *set)
 {
@@ -48,4 +24,21 @@ static char	*trim_back(char const *s1, char const *set)
 		len--;
 	}
 	return (ft_substr(s1, 0, len));
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	len = ft_strlen(s1);
+	while (*s1)
+	{
+		if (ft_strchr(set, (int)*s1) == NULL)
+			return (trim_back(s1, set));
+		s1++;
+		len--;
+	}
+	return (ft_calloc(1, 1));
 }

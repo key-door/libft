cfe251ca1d235222204ed8b9d6d344a11de32078
key-door <101494707+key-door@users.noreply.github.com>
2022-11-27 12:24:49 +0900

    commit

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+         #
+#    By: keys <keys@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/30 11:18:56 by kyoda             #+#    #+#              #
-#    Updated: 2022/09/04 01:02:30 by kyoda            ###   ########.fr        #
+#    Updated: 2022/11/27 12:24:17 by keys             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,9 +22,11 @@ ft_lstiter.c ft_lstlast.c ft_lstmap.c ft_lstnew.c ft_lstsize.c
 OBJS		= $(SRC:.c=.o)
 OBJS_BONUS	= $(SRC_BONUS:.c=.o)
 CC 			= gcc
+# CC 			= clang
 INCLUDE		= -I ./
 # INCLUDE		= -I libft.h
 CFLAGS		= -Wall -Wextra -Werror $(INCLUDE)
+# CFLAGS		= -Wall -Wextra -Werror $(INCLUDE) -g -fsanitize=address -fsanitize=undefined -fsanitize=integer
 
 ifdef WITH_BONUS
 OBJS += $(OBJS_BONUS)
--- ft_itoa.c
+++ ft_itoa.c
@@ -6,80 +6,108 @@
 /*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 13:22:39 by keys              #+#    #+#             */
-/*   Updated: 2022/11/27 11:21:23 by keys             ###   ########.fr       */
+/*   Updated: 2022/11/27 12:23:07 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// static void	itoa_ans(int n, char *str);
-
-// char	*ft_itoa_num(int n, char *str, int len)
-// {
-// 	char	*num;
-// 	int		number;
-
-// 	if (!n)
-// 		return (str);
-// 	else
-// 	{
-// 		num = n % len + '0';
-// 	}
-// 	return (ft_itoa_ans(n, ft_strjoin(num, str), len));
-// }
-
-static char	*ft_remalloc(char *str, int c)
+static void	itoa_ans(int n, char *str)
 {
-	char	*tmp;
-	size_t	len;
+	int	tmp;
+	int	n_len;
+	int	i;
 
-	len = ft_strlen(str);
-	tmp = (char *)malloc(sizeof(char) * (len + 2));
-	if (!tmp)
-		return (NULL);
-	*tmp = (char)c;
-	ft_strlcat(tmp + 1, str, len + 1);
-	free(str);
-	return (tmp);
-}
-
-static char	*ft_get_num(char *str, long n)
-{
-	int	c;
-
-	while (n)
+	i = 0;
+	tmp = n;
+	n_len = 1;
+	while (n / 10 > 0)
 	{
-		c = (n % 10) + 48;
-		n = n / 10;
-		str = ft_remalloc(str, c);
-		if (!str)
-			return (NULL);
+		n_len *= 10;
+		n /= 10;
 	}
-	return (str);
+	while (n_len > 0)
+	{
+		str[i++] = tmp / n_len + 48;
+		tmp -= tmp - (tmp % n_len);
+		n_len /= 10;
+	}
+	str[i] = '\0';
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	long	number;
-	int		flag;
+	char	str[12];
 
-	flag = 0;
-	number = (long)n;
-	if (number == 0)
-		return (ft_strdup("0"));
-	if (number < 0)
+	if (n == -2147483648)
 	{
-		flag = 1;
-		number = number * -1L;
+		str[0] = '-';
+		itoa_ans(2147483647, &str[1]);
+		str[10] = '8';
+		return (ft_strdup(str));
 	}
-	str = ft_strdup("");
-	if (!str)
-		return (NULL);
-	str = ft_get_num(str, number);
-	if (!str)
-		return (NULL);
-	if (flag == 1)
-		str = ft_remalloc(str, '-');
-	return (str);
+	if (n < 0)
+	{
+		str[0] = '-';
+		itoa_ans(n * -1, &str[1]);
+		return (ft_strdup(str));
+	}
+	itoa_ans(n, str);
+	return (ft_strdup(str));
 }
+
+// static char	*ft_remalloc(char *str, int c)
+// {
+// 	char	*tmp;
+// 	size_t	len;
+
+// 	len = ft_strlen(str);
+// 	tmp = (char *)malloc(sizeof(char) * (len + 2));
+// 	if (!tmp)
+// 		return (NULL);
+// 	*tmp = (char)c;
+// 	ft_strlcat(tmp + 1, str, len + 1);
+// 	free(str);
+// 	return (tmp);
+// }
+
+// static char	*ft_get_num(char *str, long n)
+// {
+// 	int	c;
+
+// 	while (n)
+// 	{
+// 		c = (n % 10) + 48;
+// 		n = n / 10;
+// 		str = ft_remalloc(str, c);
+// 		if (!str)
+// 			return (NULL);
+// 	}
+// 	return (str);
+// }
+
+// char	*ft_itoa(int n)
+// {
+// 	char	*str;
+// 	long	number;
+// 	int		flag;
+
+// 	flag = 0;
+// 	number = (long)n;
+// 	if (number == 0)
+// 		return (ft_strdup("0"));
+// 	if (number < 0)
+// 	{
+// 		flag = 1;
+// 		number = number * -1L;
+// 	}
+// 	str = ft_strdup("");
+// 	if (!str)
+// 		return (NULL);
+// 	str = ft_get_num(str, number);
+// 	if (!str)
+// 		return (NULL);
+// 	if (flag == 1)
+// 		str = ft_remalloc(str, '-');
+// 	return (str);
+// }

c38460bae4d8cfec07bbdf64bb4345ede65b7e4b
key-door <101494707+key-door@users.noreply.github.com>
2022-11-27 12:45:34 +0900

    commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 15:16:34 by keys              #+#    #+#             */
-/*   Updated: 2022/11/27 12:01:00 by keys             ###   ########.fr       */
+/*   Updated: 2022/11/27 12:43:37 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	void	*tmp;
 
+	tmp = dst;
 	if (dst == src || n == 0)
 		return (dst);
-	tmp = dst;
 	while (n-- != 0)
 	{
-		ft_memset(dst++, *((int *)src++), 1);
+		*(unsigned char *)dst = *(unsigned char *)src;
+		dst++;
+		src++;
 	}
 	return (tmp);
 }
